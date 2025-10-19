f9c2e9c2546fae1205eb51d7f1a5c796270e8fa9
nasir hassan <zayid04@gmail.com>
2025-10-19 03:20:22 +0000

    no side  bar dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import AppSidebar from "@/components/layout/Sidebar";
 import Header from "@/components/layout/Header";
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function DashboardLayout({
   children,
@@ -9,13 +9,15 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider>
+      <div className="flex min-h-screen">
         <AppSidebar />
-        <SidebarInset>
+        <div className="flex-1 flex flex-col">
             <Header />
-            <main className="p-4 sm:px-6 sm:py-0">
+            <main className="p-4 sm:px-6 sm:py-0 flex-1">
                 {children}
             </main>
-        </SidebarInset>
+        </div>
+      </div>
     </SidebarProvider>
   );
 }

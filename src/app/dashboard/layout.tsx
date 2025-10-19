aa64e59d12b159c44baacec0f532e90147f42fa1
nasir hassan <zayid04@gmail.com>
2025-10-19 03:17:12 +0000

    i cant see full dashbaord with side nav ,  just showing dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,12 +10,12 @@ export default function DashboardLayout({
   return (
     <SidebarProvider>
         <AppSidebar />
-        <div className="flex flex-col sm:pl-14">
-          <Header />
-          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-            {children}
-          </main>
-        </div>
+        <SidebarInset>
+            <Header />
+            <main className="p-4 sm:px-6 sm:py-0">
+                {children}
+            </main>
+        </SidebarInset>
     </SidebarProvider>
   );
 }
